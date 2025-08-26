e1b0e2b3309887b28984ac2c29249fc09a0ee04b
Anurag Negi <anurag.negi862@gmail.com>
2025-08-27 03:02:03 +0530

    const token,sol,token for sale

---
+++ programs/bonding-curve/src/constant.rs
@@ -0,0 +1,5 @@
+pub const BONDING_CURVE:u64;
+
+pub const VIRTUAL_SOL:u64=14; // 14 solana is intial virtual sol
+pub const VIRTUAL_TOKEN_RESERVE:u64=505000000; // 505 million tokens are virtual token reserve
+pub const TOKEN_FOR_SALE:u64=373000000; // 373 million tokens are for sale initially in bonding curve before migration
