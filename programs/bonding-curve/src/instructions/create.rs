3c8fde59b97683ec1727f26c65692117fdf63ef2
Anurag Negi <anurag.negi862@gmail.com>
2025-09-03 07:37:33 +0530

    sell instruction

--- programs/bonding-curve/src/instructions/create.rs
+++ programs/bonding-curve/src/instructions/create.rs
@@ -1,5 +1,5 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token::{Mint, Token, TokenAccount, Transfer,transfer};
+use anchor_spl::token::{Mint, Token, TokenAccount, Transfer, transfer, MintTo, mint_to};
 use anchor_spl::associated_token::AssociatedToken;
 use anchor_spl::token::{self,SyncNative};
 
@@ -101,17 +101,31 @@ pub fn initialize_bonding_curve(ctx:Context<InitializeBondingCurve>,fee_percenta
     let seeds=&[b"bonding-curve".as_ref(),ctx.accounts.token_mint.key().as_ref(),&[ctx.bumps.bonding_curve],];
     let signer_seeds=&[&seeds[..]];
 
+    let mint_to_bonding_curve = MintTo {
+        mint: ctx.accounts.token_mint.to_account_info(),
+        to: ctx.accounts.bonding_curve_ata.to_account_info(),
+        authority: ctx.accounts.bonding_curve.to_account_info(),
+    };
+    let cpi_ctx_bonding = CpiContext::new_with_signer(
+        ctx.accounts.token_program.to_account_info(),
+        mint_to_bonding_curve,
+        signer_seeds
+    );
+    mint_to(cpi_ctx_bonding, TOKEN_FOR_SALE)?; 
+
     let tokens_out=crate::utils::calculate_tokens_out(solAmount,virtual_sol_reserves,virtual_token_reserves)?;
     require!(tokens_out>=min_tokens_out,CustomError::SlippageTooHigh);
-    let cpi_accounts=token::MintTo{
-        mint:ctx.accounts.token_mint.to_account_info(),
-        to:ctx.accounts.user_token_ata.to_account_info(),
-        authority:ctx.accounts.bonding_curve.to_account_info()
+    let mint_to_user = MintTo {
+        mint: ctx.accounts.token_mint.to_account_info(),
+        to: ctx.accounts.user_token_ata.to_account_info(),
+        authority: ctx.accounts.bonding_curve.to_account_info()
     };
     let cpi_program=ctx.accounts.token_program.to_account_info();
-    let cpi_ctx=CpiContext::new_with_signer(cpi_program,cpi_accounts,signer_seeds);
-    token::transfer(cpi_ctx, tokens_out)?;
+    let cpi_ctx=CpiContext::new_with_signer(cpi_program, mint_to_user, signer_seeds);
+    mint_to(cpi_ctx, tokens_out)?;
     bonding_curve.virtual_sol_reserves+=solAmount;
     bonding_curve.virtual_token_reserves-=tokens_out;
+    bonding_curve.total_supply = TOKEN_FOR_SALE; 
+    
     Ok(());
 }
--- programs/bonding-curve/src/instructions/sell.rs
+++ programs/bonding-curve/src/instructions/sell.rs
@@ -4,7 +4,7 @@ use anchor_spl::token::{Transfer,Token,TokenAccount};
 
 #[derive(Accounts)]
 pub struct SellToken<'info>{
-    #[account]
+    #[account(mut)]
     pub seller:Signer<'info>,
 
     #[account(
@@ -26,6 +26,13 @@ pub struct SellToken<'info>{
 
    #[account(
     mut,
+    associated_token::mint=token_mint,
+    associated_token::authority=bonding_curve
+   )]
+   pub bonding_curve_ata:Account<'info,TokenAccount>,
+
+   #[account(
+    mut,
     payer=seller,
     associated_token::mint=token_mint,
     associated_token::authority=seller
@@ -41,5 +48,17 @@ pub fn sell_token(ctx:SellToken,min_sol_out:u64,tokens_to_sell:u64)->Result<()>{
     let mut bonding_curve=&mut ctx.accounts.bonding_curve;
     let sol_out=calculate_sol_out(tokens_to_sell)?;
     require(sol_out>=mint_sol_out,CustomError::SlippageTooHigh);
+    let cpi_accounts=Transfer{
+        from:ctx.accounts.user_token_ata.to_account_info(),
+        to:ctx.accounts.bonding_curve_ata.to_account_info(),
+        authority:ctx.accounts.seller.to_account_info()
+    };
+    let cpi_program=ctx.accounts.token_account.to_account_info();
+    let cpi_ctx=CpiContext::new(cpi_program,cpi_accounts);
+    token::transfer(cpi_ctx,tokens_to_sell)?;
+
+    **ctx.accounts.reserve_vault.to_account_info().try_borrow_mut_lamports()?-=sol_out;
+    **ctx.accounts.seller.to_account_info().try_borrow_mut_lamports()?+=sol_out;
+
     Ok(())
 }
